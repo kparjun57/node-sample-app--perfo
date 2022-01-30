b96c5907f78fd8e186daccc8f023e300acf6fe24
kparjun57 <58383660+kparjun57@users.noreply.github.com>
2022-01-30 17:27:16 +0530

    Update backend.tf

--- TF Devops - azure app service/backend.tf
+++ TF Devops - azure app service/backend.tf
@@ -2,7 +2,7 @@ terraform {
   
 backend "azurerm" {
         resource_group_name  = "terraform-state"
-        storage_account_name = "storagetraining5678"
+        storage_account_name = "storagetrain56789"
         container_name       = "terraform-state-container"
         key                  = "terraform.tfstate"
     }
