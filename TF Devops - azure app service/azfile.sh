3b3ba848309b59b2e096ac486936515988bd00f3
kparjun57 <58383660+kparjun57@users.noreply.github.com>
2022-01-30 17:26:43 +0530

    Update azfile.sh

--- TF Devops - azure app service/azfile.sh
+++ TF Devops - azure app service/azfile.sh
@@ -1,3 +1,3 @@
 az group create -l eastus2 -n terraform-state
-az storage account create -g terraform-state -l eastus2 --name storagetraining5678 --sku Standard_LRS --encryption-services blob
-az storage container create --name terraform-state-container --account-name storagetraining5678
+az storage account create -g terraform-state -l eastus2 --name storagetrain56789 --sku Standard_LRS --encryption-services blob
+az storage container create --name terraform-state-container --account-name storagetrain56789
