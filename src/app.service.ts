1bf5759e7e7b1e6c031670b4fea16cf26e264b09
kparjun57 <58383660+kparjun57@users.noreply.github.com>
2022-01-30 19:40:34 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Cloudplus testing - checking code change...';
+    return 'Hello World Cloudplus testing - 30 Jan 2022';
   }
 }
